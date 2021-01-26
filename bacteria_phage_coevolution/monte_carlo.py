3ee1c8fc7ea2b1933d4460ab037c379b1ec739a5
RID-183 <drosoulanikolopoulou@gmail.com>
2021-01-26 15:08:09 +0200

    Add user input

--- bacteria_phage_coevolution/monte_carlo.py
+++ bacteria_phage_coevolution/monte_carlo.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from math import e, exp
+from math import e, exp  
 from random import random
 
 
@@ -15,7 +15,13 @@ GenState = namedtuple(
 )
 MCParameters = namedtuple(
     "MCParameters",
-    ["growth_rate", "carrying_capacity", "burst_size", "latent_period"],
+    [
+        "growth_rate",
+        "carrying_capacity",
+        "burst_size",
+        "latent_period",
+        "secondary_decay_proportionality_constant",
+    ],
 )
 
 
@@ -68,11 +74,12 @@ def calculate_next_gen(CurrentState, SimulationParameters):
                 "infected_bacteria_count"
             ] -= 1
 
-            a = e/2
             secondary_killing = int(
                 uninfected_bacteria
                 * exp(
-                    -CurrentState.gen_count / SimulationParameters.latent_period * a
+                    -CurrentState.gen_count
+                    / SimulationParameters.latent_period
+                    * SimulationParameters.secondary_decay_proportionality_constant
                 )
             )
             carry_over[carry_over_index % carry_over_size][
@@ -104,8 +111,10 @@ def apply_carry_over(NextGen):
         "infected_bacteria_count"
     ]
 
-    secondary_killing_rate = abs(NextGen.carry_over[gen_count % carry_over_size]["uninfected_bacteria_count"] / uninfected_bacteria)
-    print(secondary_killing_rate)
+    secondary_killing_rate = abs(
+        NextGen.carry_over[gen_count % carry_over_size]["uninfected_bacteria_count"]
+        / uninfected_bacteria
+    )
     for _ in range(uninfected_bacteria):
         r = random()
         if r < secondary_killing_rate:
@@ -122,21 +131,81 @@ def apply_carry_over(NextGen):
     return UpdatedNextGen
 
 
-def monte_carlo(n):
-    SimulationParameters = MCParameters(3, 5000, 150, 3)
+def monte_carlo(n, InitialState, SimulationParameters):
+    for _ in range(n):
+        InitialState = apply_carry_over(
+            calculate_next_gen(InitialState, SimulationParameters)
+        )
+    return InitialState
+
+def get_mc_input():
+    def _get_valid_input(input_prompt, wrong_input_prompt, is_valid):
+        answer = input(input_prompt)
+        while not is_valid(answer):
+            print(wrong_input_prompt)
+            answer = input(input_prompt)
+        return answer
+
+    UNINFECTED_BACTERIA_LOWER_BOUND = 1000
+    UNINFECTED_BACTERIA_UPPER_BOUND = 10000
+
+    IsValid = namedtuple("IsValid", ["uninfected_bacteria", "moi", "growth_rate", "burst_size", "latent_period", "monte_carlo_times"])
+    MCIsValid = IsValid(
+    lambda x: int(x) in range(UNINFECTED_BACTERIA_LOWER_BOUND, UNINFECTED_BACTERIA_UPPER_BOUND + 1),
+    lambda x: float(x) in [.1, 1],
+    lambda x: float(x) >= .02 and float(x) <= 1,
+    lambda x: int(x) in range(100, 301),
+    lambda x: int(x) in range(2, 11),
+    lambda x: int(x) in range(1, 10001))
+
+    Prompt = namedtuple("Prompt", ["uninfected_bacteria", "moi", "growth_rate", "carrying_capacity", "burst_size", "latent_period", "monte_carlo_times"])
+    MCPrompt = Prompt(
+        ("Uninfected bacteria (U) [1000, 10000]: \n", "Must be in [1000, 10000]"),
+        ("Multiplicity of infection (MOI) (P/U) [0.1 | 1]: \n", "Must be either 0.1 or 1"),
+        ("Growth rate (μ) [0.2, 1]: \n", "Must be in [0.2, 1]"),
+        ("Carrying capacity (Κ) [2*U, 10*U]: \n", "Must be in [2*U, 10*U]"),
+        ("Burst size (bS) [100, 300]: \n", "Must be in [100, 300]"),
+        ("Latent period (l) [2, 10]: \n", "Must be in [2, 10*e]"),
+        ("Monte Carlo simulation times [1, 10000]: \n", "Must be in [1, 10000]")
+    )
+
+    uninfected_bacteria = int(_get_valid_input(MCPrompt.uninfected_bacteria[0], MCPrompt.uninfected_bacteria[1], MCIsValid.uninfected_bacteria))
+    moi = float(_get_valid_input(MCPrompt.moi[0], MCPrompt.moi[1], MCIsValid.moi))
+    growth_rate = float(_get_valid_input(MCPrompt.growth_rate[0], MCPrompt.growth_rate[1], MCIsValid.growth_rate))
+
+    is_valid_carrying_capacity = lambda x: int(x) >= 2*uninfected_bacteria and int(x) <= 10*uninfected_bacteria
+    carrying_capacity = int(_get_valid_input(MCPrompt.carrying_capacity[0], MCPrompt.carrying_capacity[1], is_valid_carrying_capacity))
+
+    burst_size = int(_get_valid_input(MCPrompt.burst_size[0], MCPrompt.burst_size[1], MCIsValid.burst_size))
+    latent_period = int(_get_valid_input(MCPrompt.latent_period[0], MCPrompt.latent_period[1], MCIsValid.latent_period))
+    monte_carlo_times = int(_get_valid_input(MCPrompt.monte_carlo_times[0], MCPrompt.monte_carlo_times[1], MCIsValid.monte_carlo_times))
+
+    return uninfected_bacteria, moi, growth_rate, carrying_capacity, burst_size, latent_period, monte_carlo_times
+
+def setup(user_input):
+    uninfected_bacteria, moi, growth_rate, carrying_capacity, burst_size, latent_period, monte_carlo_times = user_input
+
+    phages = int(uninfected_bacteria * moi)
+    infected_bacteria, gen_count = 0, 0
 
     carry_over = [
         {"phage_count": 0, "infected_bacteria_count": 0, "uninfected_bacteria_count": 0}
-        for _ in range(SimulationParameters.latent_period + 1)
+        for _ in range(latent_period + 1)
     ]
-    GS = GenState(1000, 1000, 0, carry_over, 0)
+    secondary_decay_proporitionality_constant = 2 if moi == .1 else e/2
 
-    for _ in range(n):
-        GS = apply_carry_over(calculate_next_gen(GS, SimulationParameters))
-    return GS
+    InitialState = GenState(uninfected_bacteria, phages, infected_bacteria, carry_over, gen_count)
+    SimulationParameters = MCParameters(growth_rate, carrying_capacity, burst_size, latent_period, secondary_decay_proporitionality_constant)
+
+    return monte_carlo_times, InitialState, SimulationParameters
 
 
-FinalState = monte_carlo(100)
-print(f"Uninfected bacteria count: {FinalState.uninfected_bacteria}")
-print(f"Phages count: {FinalState.phages}")
-print(f"Infected bacteria: {FinalState.infected_bacteria}")
+def main():
+    n, InitialState, SimulationParameters = setup(get_mc_input())
+    FinalState = monte_carlo(n, InitialState, SimulationParameters)
+
+    print(f"Uninfected bacteria count: {FinalState.uninfected_bacteria}")
+    print(f"Phages count: {FinalState.phages}")
+    print(f"Infected bacteria: {FinalState.infected_bacteria}")
+
+main()
